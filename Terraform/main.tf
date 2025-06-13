56497c0482a93be7d47229b358ff6645b3d480a6
Adrian <mihailescua@rocketmail.com>
2025-06-13 17:33:39 +0300

    config terraform file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,65 @@
-test
+provider "aws" {
+  region = "eu-west-1" # sau regiunea ta preferată
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "webhook_handler" {
+  filename         = "${path.module}/../lambda/lambda.zip"
+  function_name    = "strava_webhook_lambda"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.11"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/lambda.zip")
+}
+
+resource "aws_apigatewayv2_api" "strava_api" {
+  name          = "strava-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.strava_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.webhook_handler.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "strava_route" {
+  api_id    = aws_apigatewayv2_api.strava_api.id
+  route_key = "POST /webhook-strava"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.strava_api.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.webhook_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.strava_api.execution_arn}/*/*"
+}
